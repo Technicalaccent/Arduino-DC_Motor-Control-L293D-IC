14e9d414ef50f7cff22b9a64fd9780585f4379d7
Avinash J <57675294+Avinashjtech@users.noreply.github.com>
2020-01-20 20:31:58 +0530

    Arduino |DC_Motor |Control |L293D |IC

---
+++ 012-Arduino-DC-Motor-Control-with-L293D-Motor-Driver-IC.ino
@@ -0,0 +1,22 @@
+const int leftForward = 2;
+const int leftBackward = 3;
+const int rightForward = 4;
+const int rightBackward = 5;
+
+void setup() 
+{
+  pinMode(leftForward , OUTPUT);
+  pinMode(leftBackward , OUTPUT);
+  pinMode(rightForward , OUTPUT);
+  pinMode(rightBackward , OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(leftForward , HIGH);
+  digitalWrite(leftBackward , LOW);
+  digitalWrite(rightForward , HIGH);
+  digitalWrite(rightBackward , LOW);
+ 
+}
